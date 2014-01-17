c9c53efa2a58c578135063fcbfc8023b224f6f21
Sreekanth <sreekanth@sreekanth-desktop.(none)>
2014-01-17 22:59:33 +0530

    after formatting

--- BigProducts.php
+++ BigProducts.php
@@ -3,7 +3,6 @@
 $url= 'https://store-bzynwrh.mybigcommerce.com/api/v2/products.json?limit=200&page=1';
 //$apikey = '&apikey=864de9f6f832994db4d5ff29046a9c5595066ac3';
 
-//echo "host string " .$url."\n";
 $username='admin';
 $password='864de9f6f832994db4d5ff29046a9c5595066ac3';
 
@@ -14,15 +13,27 @@ curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
 $response = json_decode(curl_exec($ch));
+//print_r($response);
 
-//$information=curl_getinfo($process);
-//print_r($return);
-foreach ($response as $key => $value) {
-    echo "id is ".print_r ($value->id)."\n";
-    echo " name is ".print_r($value->name)."\n";
-    
-}
-//curl_close($process);
+ echo "<table id='display' align = 'center' border='1'>";
+ echo "<tr align='center'><td colspan ='3'> <b>Product Details</b></td></tr>";
+ echo "<tr> " ;
+ echo "<td><b> Prod ID </b>  </td>".    "<td> <b>ProductName</b> </td> ".    " <td><b> Price </b>  </td>";
+ echo "</tr> " ;
+ foreach ($response as $key => $value) {
+	$exp1= explode(']',$value->name,7);
+	echo "<tr> " ;
+	echo "<td> $value->id </td>" ."<td>  $exp1[1]</td>" . "<td> $value->price</td> ";
+      echo "</tr>";
+	}
+
+ echo "</table>";
+ curl_close($ch);
+?>
+
+
+
+
 
 ?>
 
